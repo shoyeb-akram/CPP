aaaffb73ef3c033fa642fcb649a4ed856a3f4ee8
Soyeb Akram <165510859+shoyeb-akram@users.noreply.github.com>
2024-04-18 02:27:16 +0600

    Add files via upload

---
+++ intro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout<< "Hello,"<< " World!\n";
+
+    cout<< "My name is Apon"<< "I am 20 years old"<< endl;
+
+
+    cout << "hi ";
+
+    
+    cout<< "My name is Apon"<< endl << "I am 20 years old";
+
+    return 0;
+}
