198bbb5214b425fe72f929d72b41e7a76f9b0ccc
Soyeb Akram <165510859+shoyeb-akram@users.noreply.github.com>
2024-04-18 02:23:48 +0600

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include <iostream> //header
+using namespace std;
+
+int main() {
+
+  cout << "Hello World! " ; 
+
+  cout << "My name is Soyeb Akram " ;   
+
+  cout << "I am a CSE student " ; 
+
+  cout << "My university name is Independent University Bangladesh " ;  
+
+  cout << "Recently I am learning c++ ";
+
+  cout << "I love to listen music ";
+
+  cout << "My hometown is Jhenaidah ";
+  cout << "But now I am living in Dhaka for studing ";
+  return 0;
+}
